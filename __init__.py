2e63c231dc30bdc723d84d600f716779b5973534
zweix123 <1979803044@qq.com>
2023-03-20 14:36:00 +0800

    improve __init__

--- __init__.py
+++ __init__.py
@@ -1,21 +1,13 @@
-"""
-Use: 
-from util import *  # import
-
-file_util.xxx  # use
-"""
-
 import os, glob
 
-# 全部导入
-current_dir = os.path.dirname(__file__)
-module_files = glob.glob(os.path.join(current_dir, "*.py"))
-module_names = [
-    os.path.basename(f)[:-3]
-    for f in module_files
-    if not f.endswith("__init__.py") and not f.endswith("test.py")
+excluded = [
+    os.path.basename(filepath)[:-3]
+    for filepath in glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
 ]
-__all__ = [module_name for module_name in module_names]
+excluded = ["__init__", "test"]
+__all__ = [
+    os.path.basename(filepath)[:-3]
+    for filepath in glob.glob(os.path.join(os.path.dirname(__file__), "*.py"))
+    if os.path.basename(filepath)[:-3] not in excluded
+]
 
-# 分别导入
-# from . import file_util, net_util...
