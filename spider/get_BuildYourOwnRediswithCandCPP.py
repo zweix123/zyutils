83b8556600fdea2aa9451ea297902d89b616f0bb
zweix123 <1979803044@qq.com>
2023-06-14 23:13:14 +0800

    develop and organize
    1. develop spider of get <built your own redis>
    2. mv script to misc
    3. modify doc

--- spider/get_BuildYourOwnRediswithCandCPP.py
+++ spider/get_BuildYourOwnRediswithCandCPP.py
@@ -3,34 +3,53 @@ from util.net_util import RailGun
 from bs4 import BeautifulSoup, Tag
 
 with RailGun() as railgun:
-    root_url = "https://build-your-own.org/redis/"  # root url
-    content_page_url = (
-        "https://build-your-own.org/redis/#table-of-contents"  # content url
-    )
+    base_url = "https://build-your-own.org/redis/"
+    content_url = "https://build-your-own.org/redis/#table-of-contents"
 
-    content_page = railgun.fire("GET", content_page_url).data  # spider
+    content_page = railgun.fire("GET", content_url).data  # get content html
 
     bs: Tag = BeautifulSoup(content_page, "html.parser")
+    # parser content page to get links
     links = [
-        root_url + li_page.get("href")
+        base_url + li_page.get("href")
         for ol_page in bs.find("div", attrs={"class": "body"}).find_all("ol")[:2]
         for li_page in ol_page.find_all("a")
     ]
-    # links is all chapter url
 
+    # loop links and get need part and aapend to sum
     sum_page = Tag(name="body")
-    for chapter_url in links:
+    for i, chapter_url in enumerate(links):
         print(chapter_url)
         chapter_page = railgun.fire("GET", chapter_url).data
         bs = BeautifulSoup(chapter_page, "html.parser")
-        sum_page.append(bs.find("div", attrs={"class": "body"}))
+        body_tag = bs.find("div", attrs={"class": "body"})
 
-    # 删除每个文章尾多余的东西
-    for codecrafters_tag in sum_page.find_all("div", attrs={"class": "codecrafters"}):
-        codecrafters_tag.extract()
+        del body_tag["class"]
 
-    bs = BeautifulSoup(content_page, "html.parser")
-    bs.find("div", attrs={"class": "body"}).replace_with(sum_page)
+        blockquote = body_tag.find("blockquote")
+        if blockquote is not None:
+            ref_base_url = "https://build-your-own.org"
+            for a_tag in blockquote.find_all("a"):
+                a_tag["class"] = "uri"
+                # 获取原始的 href 属性值
+                href = a_tag["href"]
 
-    with open("BuildYourOwnRediswithCandCPP2.html", "w") as f:
-        f.write(bs.prettify())
+                # 拼接新的 href 属性值
+                new_href = ref_base_url + href
+
+                # 更新 href 属性
+                a_tag["href"] = new_href
+
+        sum_page.append(body_tag)
+
+# unified delete thing not need
+for codecrafters_tag in sum_page.find_all("div", attrs={"class": "codecrafters"}):
+    codecrafters_tag.extract()
+
+# insert origin page
+bs = BeautifulSoup(content_page, "html.parser")
+bs.find("div", attrs={"class": "body"}).replace_with(sum_page)
+
+# write
+with open("BuildYourOwnRediswithCandCPP2.html", "w") as f:
+    f.write(str(bs))
