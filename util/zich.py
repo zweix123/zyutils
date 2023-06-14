5862e529c328d6aabbf6321495d888a7c6e423c6
zweix123 <1979803044@qq.com>
2023-06-14 11:06:21 +0800

    develop zich
    1. debug
    2. add feature of ...
    
    + color use rich

--- util/zich.py
+++ util/zich.py
@@ -2,20 +2,33 @@ import builtins
 from typing import Any, Set, List, Optional
 from inspect import ismethod, isfunction
 
+from rich import print
+
 
 class Stainer:
     def __init__(self) -> None:
         pass
 
-    def __call__(self, s: str) -> str:
+    def __call__(self, s: str) -> Any:
         return s
 
 
+class zllipsis:
+    def __init__(self, extra: str = "") -> None:
+        self.extra = extra
+
+    def __str__(self) -> str:
+        return "..." + self.extra
+
+    def __repr__(self) -> str:
+        return "..." + self.extra
+
+
 class Entry:
-    def __init__(self, k: Any, v: Any, sep: str) -> None:
+    def __init__(self, k: Any, v: Any = None, sep: str = "") -> None:
         # if dict: k is dict key, v is dict value, sep is ' : '
         # if custom: k is attr name, v is attr value, sep is " = "
-        # if ...: k is None, v is None, sep is not key
+        # if ...: k is zllipsis, v and sep is not key
         self.k = k
         self.v = v
         self.sep = sep
@@ -26,6 +39,7 @@ class Zish:
 
     LIMIT = 4  # 对于非字符串可序列化的元素的个数限制
     MAXN = 80  # 对于可str化的单行长度限制
+    ELEMENTS = 7  # 借用objprint的名字, 实际含义是允许递归最大深度
     TAB = "  "
     BANS: Set[Any] = set()  # 特判不会处理的custom class
 
@@ -46,12 +60,10 @@ class Zish:
         comma: Optional[bool] = None,  # 是否输出逗号, None则遵循当前层次全局配置
         end: Optional[str] = None,  # print函数的end参数, None则遵循当前层次全局配置
     ) -> None:
-        if indent is None:
-            indent = self.config[-1]["indent"]
-        if comma is None:
-            comma = self.config[-1]["comma"]
-        if end is None:
-            end = self.config[-1]["end"]
+        config = self.config[-1]
+        indent = indent if indent is not None else config["indent"]
+        comma = comma if comma is not None else config["comma"]
+        end = end if end is not None else config["end"]
 
         lines = s.splitlines()
         if len(lines) > 0:
@@ -60,7 +72,6 @@ class Zish:
             for i in range(1, len(lines)):
                 lines[i] = (self.cur_dep + 1) * Zish.TAB + lines[i]
         s = "\n".join(lines)
-
         Zish._print(
             "{}{}{}".format(
                 "",  # self.cur_dep * Zish.TAB if indent is True else "",
@@ -82,6 +93,7 @@ class Zish:
         )
 
     def __call__(self, *insts: Any, depth: int = 0) -> Any:
+        assert depth <= Zish.ELEMENTS, "循环依赖!, 寄"
         # 转递归为迭代
         if len(insts) == 1:
             inst = insts[0]
@@ -93,12 +105,17 @@ class Zish:
         last_dep = self.cur_dep
         self.cur_dep = depth
 
-        if Zish.check_builtin(inst):
+        if isinstance(inst, zllipsis):
+            self.handle_zllipsis(inst)
+        elif Zish.check_builtin(inst):
             self.handle_builtin(inst)
         else:
             self.handle_custom(inst)
 
         self.cur_dep = last_dep
+
+    def handle_zllipsis(self, inst: zllipsis) -> None:
+        self.print(str(inst), end="\n")
 
     @staticmethod
     def check_single(inst: Any) -> bool:
@@ -116,22 +133,22 @@ class Zish:
 
     def gen_single(self, inst: Any) -> str:
         if isinstance(inst, type(...)):
-            return "..."
+            return str(zllipsis())
         elif isinstance(inst, str):
             if len(inst) <= Zish.MAXN:
                 return f"'{inst}'"
-            if len(inst) > Zish.MAXN:
-                return f"str('{inst[:Zish.MAXN]}...' len={len(inst)})"
+            return "str('{}{}".format(
+                inst[: Zish.MAXN], str(zllipsis(f"' len={len(inst)})"))
+            )
         elif isinstance(inst, (bytes, bytearray)):
             if len(str(inst)) <= Zish.MAXN:
                 return str(inst)
-            else:
-                handle = hex  # int 10; bin 2; oct 8, hex 16
-                res = f"{type(inst).__name__}(["
-                for byte in inst[: int(Zish.MAXN // 4 - 2)]:
-                    res += handle(byte) + " "
-                res += f"... {handle(inst[-1])}] len={len(inst)})"
-                return res
+            handle = hex  # int 10; bin 2; oct 8, hex 16
+            return "{}{} {}".format(
+                f"{type(inst).__name__}([",
+                " ".join([handle(byte) for byte in inst[: int(Zish.MAXN // 4 - 2)]]),
+                str(zllipsis(f" {handle(inst[-1])}] len={len(inst)})")),
+            )
         return str(inst)
 
     def handle_custom(self, inst: Any) -> None:
@@ -139,12 +156,13 @@ class Zish:
         if False:  # 占位
             pass
         elif hasattr(type(inst), "__str__") and type(inst).__str__ is not object.__str__:  # fmt: skip
-
-            self.print(f"{type(inst).__name__}(\n{inst.__str__()}", end="\n")
-            self.print(")")
+            self.print(inst.__str__())
+            # self.print(f"{type(inst).__name__}(\n{inst.__str__()}", end="\n")
+            # self.print(")", indent=True)
         elif hasattr(type(inst), "__repr__") and type(inst).__repr__ is not object.__repr__:  # fmt: skip
-            self.print(f"{type(inst).__name__}(\n{inst.__repr__()}", end="\n")
-            self.print(")")
+            self.print(inst.__repr__())
+            # self.print(f"{type(inst).__name__}(\n{inst.__repr__()}", end="\n")
+            # self.print(")", indent=True)
         else:
             self.handle_multi(inst)
 
@@ -157,28 +175,44 @@ class Zish:
             self.handle_com(inst)
 
     def handle_entry(self, inst: Entry) -> None:
-        if inst.k is None and inst.v is None:
-            self.print(self.gen_single(...))
+        if isinstance(inst.k, zllipsis):
+            self.handle_zllipsis(inst.k)
             return
-
+        # key
         if Zish.check_builtin(inst.k) and Zish.check_single(inst.k):
             self.print(self.gen_single(inst.k), comma=False, end="")
-            self.print(inst.sep, indent=False, comma=False, end="")
         else:
             self.config.append(Zish.gen_config(True, False, ""))
             self(inst.k, depth=self.cur_dep)
             self.config.pop()
-            self.print(inst.sep, indent=False, comma=False, end="")
+        # ' : '
+        self.print(inst.sep, indent=False, comma=False, end="")
 
+        # value
         self.config.append(Zish.gen_config(False, True, end="\n"))
         self(inst.v, depth=self.cur_dep)
         self.config.pop()
 
-    def handle_seq(self, inst: Any) -> None:
+    def handle_seq(self, inst: list | tuple | set) -> None:
+        symbol = {"list": ["[", "]"], "tuple": ["(", ")"], "set": ["{", "}"]}[type(inst).__name__]  # fmt: skip
+        if (s := self.gen_sep(inst)) is not None:
+            self.print(s)
+            return
+        content = list(inst)  # 这里转换成list是因为tuple不能修改、set不能索引访问
+        if len(content) > Zish.LIMIT:
+            content[Zish.LIMIT :] = [zllipsis(f" len={len(inst)}")]
+
+        self.print(symbol[0], comma=False, end="\n")
+        self.config.append(Zish.gen_config(True, True))
+        zp(*content, depth=self.cur_dep + 1)
+        self.config.pop()
+        self.print(symbol[1], indent=True, comma=True)
+
+    def gen_sep(self, inst: list | tuple | set) -> Optional[str]:
         symbol = {"list": ["[", "]"], "tuple": ["(", ")"], "set": ["{", "}"]}[type(inst).__name__]  # fmt: skip
         if all(Zish.check_builtin(ele) and Zish.check_single(ele) for ele in inst):
             if len(str(inst)) <= Zish.MAXN:
-                self.print(str(inst))
+                return str(inst)
             else:
                 ele_str_list: List[str] = list()
                 for ele in inst:
@@ -186,24 +220,14 @@ class Zish:
                     if len(", ".join([ele_str for ele_str in ele_str_list])) > Zish.MAXN:  # fmt: skip
                         break
                     ele_str_list.append(ele_str)
-                if len(ele_str_list) > Zish.LIMIT:
-                    ele_str_list.append(self.gen_single(...))
-                    self.print(symbol[0]+ ", ".join([ele_str for ele_str in ele_str_list])+ symbol[1])  # fmt: skip
-                else:
-                    self.print(symbol[0], comma=False, end="\n")
-                    self.config.append(Zish.gen_config(True, True))
-                    zp(*list(inst)[:3], depth=self.cur_dep + 1)  # 这里转换成list是因为set不能索引访问
-                    self.config.pop()
-                    self.print(symbol[1], indent=True, comma=True)
-        else:
-            content = list(inst)
-            if len(content) > Zish.LIMIT:
-                content[Zish.LIMIT :] = [...]
-            self.print(symbol[0], comma=False, end="\n")
-            self.config.append(Zish.gen_config(True, True))
-            zp(*content, depth=self.cur_dep + 1)  # 这里转换成list是因为set不能索引访问
-            self.config.pop()
-            self.print(symbol[1], indent=True, comma=True)
+                if len(inst) > len(ele_str_list):  # 还有多个元素没纳入
+                    ele_str_list.append(str(zllipsis(f" len={len(inst)}")))
+                return (
+                    symbol[0]
+                    + ", ".join([ele_str for ele_str in ele_str_list])
+                    + symbol[1]
+                )
+        return None
 
     def handle_com(self, inst: Any) -> None:
         entries: List[Entry] = list()
@@ -212,8 +236,8 @@ class Zish:
                 entries.append(Entry(k, v, " : "))
                 if len(entries) >= Zish.LIMIT:
                     break
-            if len(inst.items()) > Zish.LIMIT:
-                entries.append(Entry(None, None, "..."))
+            if len(inst.items()) > len(entries):
+                entries.append(Entry(zllipsis(f" len={len(inst)}")))
             self.print("{", comma=False, end="\n")
             self.config.append(Zish.gen_config(True, True))
             self(*entries, depth=self.cur_dep + 1)
@@ -226,14 +250,21 @@ class Zish:
                     continue
                 entries.append(Entry(attr, value, " = "))
 
-            self.print(f"<{type(inst).__name__}")
+            self.print(f"<{type(inst).__name__}", comma=False, end="\n")
             self.config.append(Zish.gen_config(True, True))
             self(*entries, depth=self.cur_dep + 1)
             self.config.pop()
-            self.print(">")
+            self.print(">", indent=True)
 
 
 zp = Zish()
+
+
+# from objprint import op, config
+
+# config(color=True)
+
+# zp = op
 
 if __name__ == "__main__":
     from enum import Enum, auto
@@ -268,16 +299,11 @@ if __name__ == "__main__":
     zp(list(range(100)))
     zp(list([str(i) * i for i in range(1, 100)]))
     zp(list([str(i) * i for i in range(100, 0, -1)]))
-    zp(
-        [
-            [1, 2, 3, 4, 5, 6],
-            [1, 2, 3, 4, 5, 6],
-            [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6],
-            [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6],
-        ]
-    )
+    zp([[i + j for j in range(10)] for i in range(10)])
+    zp([[1], [2], [3]])
     zp({1: "1", 2: "2", 3: "3"})
     zp({"1": 1, "2": 2, "3": 3})
+    zp({i: str(i) for i in range(100)})
 
     class Time(Enum):
         SECOND = 1
@@ -326,15 +352,10 @@ if __name__ == "__main__":
         r = list(range(100))
         s = list([str(i) * i for i in range(1, 100)])
         t = list([str(i) * i for i in range(100, 0, -1)])
-        u = [
-            [1, 2, 3, 4, 5, 6],
-            [1, 2, 3, 4, 5, 6],
-            [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6],
-            [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6],
-        ]
-
+        u = [[i + j for j in range(10)] for i in range(10)]
         v = {1: "1", 2: "2", 3: "3"}
         w = {"1": 1, "2": 2, "3": 3}
+        ww = {i: str(i) for i in range(100)}
         x = Time.HOUR
         y = {
             DogType.A: [1, "2", 3],
@@ -348,18 +369,7 @@ if __name__ == "__main__":
 
     zp(T())
 
-    class Join:
-        def __init__(self, a, b, c) -> None:
-            self.a = a
-            self.b = b
-            self.c = c
-
-        def __repr__(self) -> str:
-            return f"""{self.a}
-{self.b}
-{self.c}"""
-
-    j = Join(1, 2, 3)
-
-    zp(j)
-    zp([j, j, j])
+    # a = []
+    # b = [a]
+    # a.append(b)
+    # zp(a)
